022cec5b20d9df1253b808e34c5a9531d8013e33
Nityaa1802 <nitya.agarwal_cs.aiml20@gla.ac.in>
2023-04-06 16:18:03 +0530

    Initial commit

---
+++ src/main/java/com/example/shadow_project/ShadowProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.shadow_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShadowProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShadowProjectApplication.class, args);
+    }
+
+}
