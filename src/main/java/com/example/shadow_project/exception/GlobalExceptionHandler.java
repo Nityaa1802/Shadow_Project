7a9a71025710d7367424388e3549705e54f20fee
Samarth Porwal <samarthkporwal@gmail.com>
2023-04-19 02:07:38 +0530

    Resolved issue #5
    Apply validation for user details and handle exceptions.

--- src/main/java/com/example/shadow_project/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/shadow_project/exception/GlobalExceptionHandler.java
@@ -3,8 +3,13 @@ package com.example.shadow_project.exception;
 import com.example.shadow_project.Payload.ApiResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -14,6 +19,17 @@ public class GlobalExceptionHandler {
         String message = ex.getMessage();
         ApiResponse apiResponse = new ApiResponse(message,false);
         return new ResponseEntity<ApiResponse>(apiResponse, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> handleMethodArgsNotValidException(MethodArgumentNotValidException ex){
+        Map<String, String> response = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error)->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            response.put(fieldName,message);
+        });
+        return new ResponseEntity<Map<String,String>>(response,HttpStatus.BAD_REQUEST);
     }
 
 }
