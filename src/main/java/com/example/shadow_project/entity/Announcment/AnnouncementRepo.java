2af2d106cc36c2c8b11919da1443cbdbec2c3ae1
Samarth Porwal <samarthkporwal@gmail.com>
2023-06-02 09:34:29 +0530

    #14 Created Upload Api's
    #14 Created Upload Api's

---
+++ src/main/java/com/example/shadow_project/entity/Announcment/AnnouncementRepo.java
@@ -0,0 +1,6 @@
+package com.example.shadow_project.entity.Announcment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnnouncementRepo extends JpaRepository<Announcement,Long> {
+}
