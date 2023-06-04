0c36002bc1dc0cbcd1707aa34b3e96f5691c12ac
Samarth Porwal <samarthkporwal@gmail.com>
2023-06-04 13:54:19 +0530

    #28 created api to fetch top 6 announcements
    #28 created api to fetch top 6 announcements

--- src/main/java/com/example/shadow_project/entity/Announcment/AnnouncementController.java
+++ src/main/java/com/example/shadow_project/entity/Announcment/AnnouncementController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/announcement")
 public class AnnouncementController {
@@ -20,5 +22,10 @@ public class AnnouncementController {
     public ResponseEntity<Announcement> getAnnouncement(@PathVariable("announcementId") Long id){
         Announcement announcement = announcementService.getAnnouncement(id);
         return ResponseEntity.ok(announcement);
+    }
+    @GetMapping("/latestAnnouncements")
+    public ResponseEntity<List<Announcement>> getTop6Announcements() throws Exception {
+        List<Announcement> announcementList = announcementService.top6Announcements();
+        return ResponseEntity.ok(announcementList);
     }
 }
